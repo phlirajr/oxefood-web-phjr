3cf2be553dce52de238f5afb050bfa753d1fbd92
aluno <aluno@ifpe>
2023-10-26 09:48:57 -0300

    Controle de Acesso

--- src/MenuSistema.jsx
+++ src/MenuSistema.jsx
@@ -15,46 +15,53 @@ class MenuSistema extends React.Component{
            <>
                <Menu inverted>
                   
-                   <Menu.Item
-                       name='home'
-                       active={this.state.activeItem === 'home'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/'
-                   />
-                   <Menu.Item
-                       name='cliente'
-                       active={this.state.activeItem === 'cliente'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-cliente'
-                   />
+                    <Menu.Item
+                        name='home'
+                        active={this.state.activeItem === 'home'}
+                        onClick={this.handleItemClick}
+                        as={Link}
+                        to='/home'
+                    />
+                    <Menu.Item
+                        name='cliente'
+                        active={this.state.activeItem === 'cliente'}
+                        onClick={this.handleItemClick}
+                        as={Link}
+                        to='/list-cliente'
+                    />
 
-                   <Menu.Item
-                       name='produto'
-                       active={this.state.activeItem === 'produto'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-produto'
-                   />
+                    <Menu.Item
+                        name='produto'
+                        active={this.state.activeItem === 'produto'}
+                        onClick={this.handleItemClick}
+                        as={Link}
+                        to='/list-produto'
+                    />
 
-                   <Menu.Item
-                       name='entregador'
-                       active={this.state.activeItem === 'entregador'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-entregador'
-                   />
+                    <Menu.Item
+                        name='entregador'
+                        active={this.state.activeItem === 'entregador'}
+                        onClick={this.handleItemClick}
+                        as={Link}
+                        to='/list-entregador'
+                    />
 
-                   <Menu.Item
-                       name='cupom de desconto'
-                       active={this.state.activeItem === 'cupom de desconto'}
-                       onClick={this.handleItemClick}
-                       as={Link}
-                       to='/list-cupom'
-                   />
+                    <Menu.Item
+                        name='cupom de desconto'
+                        active={this.state.activeItem === 'cupom de desconto'}
+                        onClick={this.handleItemClick}
+                        as={Link}
+                        to='/list-cupom'
+                    />
+                    <Menu.Item
+                        className='navbar__item--mobile'
+                        onClick={this.logout}
+                        content='Sair'
+                        as={Link}
+                        to='/'
+                    />
 
-               </Menu>
+                </Menu>
            </>
        )
    }
--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,30 +2,65 @@ import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
 import FormCliente from './views/client/FormCliente';
-import Home from './views/home/Home';
 import ListCliente from './views/client/ListCliente';
-import ListEntregador from './views/entregador/ListEntregador';
-import ListProduto from './views/produto/ListProduto';
+import Home from './views/home/Home';
+import FormLogin from './views/login/FormLogin';
 import FormProduto from './views/produto/FormProduto';
-import FormEntregador from './views/entregador/FormEntregador';
-import FormCupomDesconto from './views/cupom/FormCupomDesconto';
-import ListCupomDesconto from './views/cupom/ListCupomDesconto';
+import ListProduto from './views/produto/ListProduto';
+import { ProtectedRoute } from './views/util/ProtectedRoute';
 
 function Rotas() {
     return (
-        <>
-            <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="list-cliente" element={ <ListCliente/> } />
-                <Route path="form-cliente" element={ <FormCliente/> } />
-                <Route path="list-produto" element={ <ListProduto/> } />
-                <Route path="form-produto" element={ <FormProduto/> } />
-                <Route path="list-entregador" element={ <ListEntregador/> }/>
-                <Route path="form-entregador" element={ <FormEntregador/> }/>
-                <Route path="form-cupom" element={ <FormCupomDesconto/> } />
-                <Route path="list-cupom" element={ <ListCupomDesconto/> } />
-            </Routes>
-        </>
+        <Routes>
+            
+            <Route path="/" element={ <FormLogin/> } />
+
+            <Route
+                    path="/home"
+                    element={
+                    <ProtectedRoute>
+                        <Home />
+                    </ProtectedRoute>
+                    }
+                />
+                
+                <Route
+                    path="/list-cliente"
+                    element={
+                    <ProtectedRoute>
+                        <ListCliente />
+                    </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/form-cliente"
+                    element={
+                    <ProtectedRoute>
+                        <FormCliente />
+                    </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/list-produto"
+                    element={
+                    <ProtectedRoute>
+                        <ListProduto />
+                    </ProtectedRoute>
+                    }
+                />
+
+                <Route
+                    path="/form-produto"
+                    element={
+                    <ProtectedRoute>
+                        <FormProduto />
+                    </ProtectedRoute>
+                    }
+                />
+
+        </Routes>
     )
 }
 
